0fd4dcebf591412752f23040d7ff84fdd6c4a4ad
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-04-15 03:53:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Simple Resume Prompt
+
+The application parses a pdf file, and feed the context to google `Gemini` AI to provide you with some context about the cv.
+
+## Installation:
++ clone the repo
++ head over to the project and initialize your environment variable
+   - run `python3 -m venv <venv-name>`
+   - run  `source <venv-name>/bin/activate`
+   - run `pip install -r requirements.txt`
++ head over to [google](https://aistudio.google.com/app/apikey) and generate an api key, and put it in your environment variable under the name `GOOGLE_API_KEY`.
+
+## How to use:
+
+The application assumes there's a `6.pdf` in your project tree, feel free to change that.</b>
+run ` GOOGLE_API_KEY=<your-api-key> python3 main.py` then you'll have your prompt initialized, you can ask the AI anyhting about the provided resume.
---
+++ main.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+import os
+import cmd
+import google.generativeai as genai
+from pdfminer.high_level import extract_text
+from google.auth import sys
+"""
+At the command line, only need to run once to install the package via pip:
+
+$ pip install google-generativeai
+"""
+
+# convo = model.start_chat()
+
+
+class Prompt(cmd.Cmd):
+    '''prompt'''
+    prompt = '(User:) '
+
+    def __init__(self):
+        '''Initialze the model and prompt'''
+        super().__init__()
+        genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+        # Set up the model
+        generation_config = {
+          "temperature": 0.9,
+          "top_p": 1,
+          "top_k": 1,
+          "max_output_tokens": 2048,
+        }
+
+        safety_settings = [
+          {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+          },
+          {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+          },
+          {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+          },
+          {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+          },
+        ]
+
+        self.model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                                           generation_config=generation_config,
+                                           safety_settings=safety_settings)
+
+    def precmd(self, line):
+        '''Runs before and command'''
+        print('(Gemeni:) ', end='')
+        sys.stdout.flush()
+        return line
+
+    def emptyline(self):
+        """Handle empty line by doing nothing"""
+
+    def do_quit(self, _):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, _):
+        """End-of-file"""
+        return True
+
+    def default(self, line):
+        '''ask gemeni and generate a response.
+        based on the pdf file.
+
+        Args:
+            line: prompt to provide for gemini
+        Notes:
+            the function assumes there's a pdf file called 6.pdf
+            feel free to change this to obtain some info about the the cf
+        '''
+        input_ = extract_text("6.pdf").strip()
+        resp = self.model.generate_content([line, input_], stream=True)
+        for chunk in resp:
+            print(chunk.text)
+
+
+if __name__ == '__main__':
+    Prompt().cmdloop()
